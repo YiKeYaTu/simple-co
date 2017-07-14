6dd93091c1542fde7c6fe6b8ceeae1e5d3325419
chenyang.lc <chenyang.lc@alibaba-inc.com>
2017-07-14 22:20:35 +0800

    feat: 新增传参功能

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # 一个简易的 generator 执行器
+
+```javascript
+  const co = require('./');
+
+  co(function* () {
+    const a = Promise.resolve(1);
+    const b = yield Promise.all([1, 3, a]);
+
+    return b;
+  }).then(
+    ret => console.log(ret),
+    err => console.log(err)
+  );
+
+```
--- index.js
+++ index.js
@@ -1,19 +1,1 @@
 module.exports = require('./lib');
-
-module.exports(function* () {
-  var a = yield new Promise(function(resolve, reject) {
-    setTimeout(resolve(100));
-  });
-  var b = yield step1(2442);
-
-  return a + b;
-}).then(
-  ret => console.log(ret),
-  err => console.log(err)
-);
-
-function* step1 (num) {
-  return yield new Promise(function(resolve, reject) {
-    setTimeout(resolve(num));
-  });
-}
