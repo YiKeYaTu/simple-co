706ec368f2de721c88f6012d5906e7fa0a97814d
chenyang.lc <chenyang.lc@alibaba-inc.com>
2017-07-14 19:48:22 +0800

    feat: 新增对 generator 的 yield 支持

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 module.exports = require('./lib');
+
+module.exports(function* () {
+  var a = yield new Promise(function(resolve, reject) {
+    setTimeout(resolve(100));
+  });
+  var b = yield step1(2442);
+
+  return a + b;
+}).then(
+  ret => console.log(ret),
+  err => console.log(err)
+);
+
+function* step1 (num) {
+  return yield new Promise(function(resolve, reject) {
+    setTimeout(resolve(num));
+  });
+}
